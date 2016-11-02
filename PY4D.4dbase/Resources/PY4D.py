6db6b6a82bc37437a3b7ec4141a4ecc59f2ca131
TimTTse <tim_t_tse@yahoo.com>
2016-11-02 13:41:41 -0700

    Reworked PY4D.py so it works with python 2.7 and 3.5

--- PY4D.4dbase/Resources/PY4D.py
+++ PY4D.4dbase/Resources/PY4D.py
@@ -1,7 +1,6 @@
 import imp, sys
 import json
 import os.path
-# from types import *
 from json import JSONEncoder
 from collections import OrderedDict
 
@@ -9,52 +8,108 @@ try:
 	import __builtin__
 except(NameError,ImportError):
 	import builtins
-# import builtins
 
-## get array of valid 4D types
-def getValid4DTypes(n=True,s=True,b=True):
-	numTypes=[int,float]
-	strTypes=[str,bytes,unicode]
-	boolTypes=[bool]
-	return (numTypes*n)+(strTypes*s)+(boolTypes*b)
+class PY4D:
+	def __init__(self,pyVer="2",moduleName="",path="",func="",params={}):
+		self.pyVer=pyVer
+		self.moduleName=moduleName
+		self.path=path
+		self.func=func
+		self.params=params
 
 
-## get list type if all values are same, else return False
-## Python types: https://docs.python.org/2/library/types.html
-def getListType(seq):
-    iseq = iter(seq)
-    firstType = type(next(iseq))
-    numTypes=getValid4DTypes(True,False,False)
-    strTypes=getValid4DTypes(False,True,False)
+		## get array of valid 4D types
+	def getValid4DTypes(self,n=True,s=True,b=True):
+		numTypes=[int,float]
+		if (self.pyVer=="2"):
+			strTypes=[str,unicode]
+		else:
+			strTypes=[str,bytes]
+		boolTypes=[bool]
+		return (numTypes*n)+(strTypes*s)+(boolTypes*b)
 
-    if firstType in numTypes:
-    	return firstType if all( (type(x) in numTypes) for x in iseq ) else False
 
-    if firstType in strTypes:
-    	return firstType if all( (type(x) in strTypes) for x in iseq ) else False
+	## get list type if all values are same, else return False
+	## Python types: https://docs.python.org/2/library/types.html
+	def getListType(self,seq):
+	    iseq = iter(seq)
+	    firstType = type(next(iseq))
+	    numTypes=self.getValid4DTypes(True,False,False)
+	    strTypes=self.getValid4DTypes(False,True,False)
 
-    return firstType if all( (type(x) is firstType) for x in iseq ) else False
+	    if firstType in numTypes:
+	    	return firstType if all( (type(x) in numTypes) for x in iseq ) else False
 
-def evalMethod(module,func,params):
-	try:
-		methodToCall= getattr(module,func)
-		p=[v for v in params.values()]
-		execString="methodToCall(*p)"
-		return eval(execString)
-	except(AttributeError):
-		exec(func)
-		return None
+	    if firstType in strTypes:
+	    	return firstType if all( (type(x) in strTypes) for x in iseq ) else False
 
-def evalBuiltIn(pyVer,func,params):
-	if (pyVer=="2"):
-		return evalMethod(__builtin__,func,params)
+	    return firstType if all( (type(x) is firstType) for x in iseq ) else False
+
+	def evalMethod(self,module,func,params):
+		try:
+			methodToCall= getattr(module,func)
+			p=[v for v in params.values()]
+			return eval("methodToCall(*p)")
+		except(AttributeError):
+			exec(func)
+			return None
+
+	def evalBuiltIn(self,func,params):
+		if (self.pyVer=="2"):
+			return self.evalMethod(__builtin__,func,params)
+		else:
+			return self.evalMethod(builtins,func,params)
+
+	def execute(self):
+		moduleName=self.moduleName
+		func=self.func
+		path=self.path
+		params=self.params
+
+		if moduleName=="" and func!="":
+			ret=self.evalBuiltIn(func,params)
+
+		else:
+			scriptFolder=os.path.abspath(os.path.join(path,os.pardir)) # script file's parent directory
+			sys.path.insert(0,scriptFolder) # Add modules located at script location   
+			foo = imp.load_source(moduleName,path)
+			ret=self.evalMethod(foo,func,params)
+
+		js=JSONEncoder()
+		if(ret!=None):
+			retType=type(ret)
+			appendObjStr=''
+			if retType==list or retType==tuple:
+				listElemType=self.getListType(ret)
+
+				if listElemType:  ## If all list elements are the same type
+					appendObjStr='","ElemType":"'+str(listElemType)+'"'
+
+					if listElemType in self.getValid4DTypes(False,True,False): ## if all list elements are of string or unicode type
+						ret=[str(i) for i in ret]						  ## convert to string (if unicode)
+				else:
+					appendObjStr='"'
+
+			else:
+				appendObjStr='"'
+
+
+			if (retType not in self.getValid4DTypes()):
+				ret=str(ret)
+
+			retTypeString=str(retType)
+			resObj='{"Return":' + js.encode(ret)+', "Type":"'+retTypeString+appendObjStr+'}'
+			print (resObj)
+
+if __name__=="__main__":
+
+	pyVer=sys.version[0]
+	if (pyVer=='2'):
+		args=raw_input()
+		args=json.loads(args.decode('utf-8','ignore'),object_pairs_hook=OrderedDict)
 	else:
-		return evalMethod(builtins,func,params)
-
-def main(args,pyVer):
-	# args=json.loads(args.decode('utf-8','ignore'),object_pairs_hook=OrderedDict) # parse json
-	args=json.loads(args.decode('utf-8','ignore'),object_pairs_hook=OrderedDict)
-	# args=json.loads(args,object_pairs_hook=OrderedDict)
+		args=input()
+		args=json.loads(args,object_pairs_hook=OrderedDict)
 
 	moduleName=args["ModuleName"]
 	path=args["Path"]
@@ -64,50 +119,6 @@ def main(args,pyVer):
 	else:
 		params={}
 
-	if moduleName=="" and func!="":
-		# ret=evalBuiltIn(func,params)
-		ret=evalBuiltIn(pyVer,func,params)
-
-	else:
-
-		scriptFolder=os.path.abspath(os.path.join(path,os.pardir)) # script file's parent directory
-		sys.path.insert(0,scriptFolder) # Add modules located at script location   
-		foo = imp.load_source(moduleName,path)
-		ret=evalMethod(foo,func,params)
-
-	js=JSONEncoder()
-	if(ret!=None):
-		retType=type(ret)
-		appendObjStr=''
-		if retType==list or retType==tuple:
-			listElemType=getListType(ret)
-
-			if listElemType:  ## If all list elements are the same type
-				appendObjStr='","ElemType":"'+str(listElemType)+'"'
-
-				if listElemType in getValid4DTypes(False,True,False): ## if all list elements are of string or unicode type
-					ret=[str(i) for i in ret]						  ## convert to string (if unicode)
-			else:
-				appendObjStr='"'
-
-		else:
-			appendObjStr='"'
-
-
-		if (retType not in getValid4DTypes()):
-			ret=str(ret)
-
-		retTypeString=str(retType)
-		resObj='{"Return":' + js.encode(ret)+', "Type":"'+retTypeString+appendObjStr+'}'
-		print (resObj)
-
-if __name__=="__main__":
-
-	pyVer=sys.version[0]
-	if (pyVer=='2'):
-		args=raw_input()
-	else:
-		args=input()
-
-	main(args,pyVer)
+	p=PY4D(pyVer,moduleName,path,func,params)
+	p.execute()
 	
--- README.md
+++ README.md
@@ -8,4 +8,16 @@ $2 - input function name<br/>
 $3 - Pointer to  variable to contain result, or "*" if no results<br/>
 ${4} - Pointer to parameter(s) (optional)<br/>
 $0 - error, or "" if no error<br/>
+<br/><br/>
+Tested for Python versions:<br/>
+2.7.12<br/>
+3.5.2<br/>
 <br/>
+Works in:<br/>
+2.7.x, 3.3.x, 3.4.x, 3.5.x<br/>
+<br/>
+Compatible (not all features work)<br/>
+3.1.x, 3.2.x<br/>
+<br/>
+Does not work:<br/>
+2.6.x, 3.0.x<br/>
